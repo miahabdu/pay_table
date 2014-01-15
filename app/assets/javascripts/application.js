24fe165edfce7038a3a364eeebf9cb57d94a3c70
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-01-14 21:46:04 -0500

    dev make edit in place work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.purr
+//= require best_in_place
 //= require jquery.ui.all
 //= require twitter/bootstrap
 //= require turbolinks
