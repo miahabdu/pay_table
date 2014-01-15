2997481071bc1633fe47151f8c455b30576d5e0e
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-01-14 19:42:46 -0500

    master inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PayTable::Application.config.secret_key_base = '583fd2f47169f164f3e11e8b16fac127510725f7848a70511f03daa5cd140574802b6291363ef4d5b83fac961c723401aa7a90e2726cc29e67e56a6db532b089'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PayTable::Application.config.session_store :cookie_store, key: '_pay_table_session'
