ad0bdf0043c4f9f88e7c06614286a032c81137da
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-01-15 21:00:59 -0500

    dev add code that duplicates last months accounts and updates due_date

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,44 @@
 class Account < ActiveRecord::Base
+
+  @account ||= self.last
+  def self.update_due_date
+    all.each do |acc|
+      @account = acc
+      if first_of_the_month? && due_date_is_valid?
+        create_dup_record
+      end
+    end
+  end
+
+private
+  def self.due_date_is_valid?
+    due_date_is_last_month || due_date_is_last_dec
+  end
+
+  def self.create_dup_record
+    x = @account.dup
+    x.due_date = x.due_date + 1.month
+    x.save
+  end
+
+  def self.due_date_is_last_month
+    @account.due_date.month == last_month && @account.due_date.year == current_year
+  end
+
+  def self.due_date_is_last_dec
+    @account.due_date.month == 12 && @account.due_date.year == current_year
+  end
+
+  def self.last_month
+    Date.today.ago(1.month).month
+  end
+
+  def self.current_year
+    Date.today.year - 1
+  end
+
+  def self.first_of_the_month?
+    Date.today == Date.today.beginning_of_month
+  end
+
 end
