226b5f9a0812e4bce1278c59b614cce6ea6bb57c
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-01-21 20:03:52 -0500

    master fix user editing for devise(allow parameters)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!
   protect_from_forgery with: :exception
-
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+    
   layout :layout
 
-  private
+  protected
 
   def layout
     # only turn it off for login pages:
     is_a?(Devise::SessionsController) ? false : "application"
     # or turn layout off for every devise controller:
     !devise_controller? && "application"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit :username, :email, :password, :password_confirmation, :current_password
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
  
-  attr_accessor :login
-
   has_many :accounts
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      where(conditions).first
-    end
-  end
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
